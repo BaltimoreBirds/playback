616b328519b1a77bc0ec1e52de01308b91a43800
Michael Swanson <mikeswanson12@msn.com>
2013-08-22 14:32:36 -0400

    playback done

--- playback.rb
+++ playback.rb
@@ -2,7 +2,22 @@ puts "What do you want to say?"
 response = gets.chomp
 
 def playback(string)
-  puts "You said: #{string}"
+  if string.downcase == "nothing"
+    return "Ok, fine!"
+  elsif string.downcase == "i have a lot to say"
+    input_array = []
+    puts "Ok, let's hear it!"
+    while string.downcase != "done"
+      string = gets.chomp
+      input_array << string
+    end
+    input_array.pop
+    input_array.each do |word|
+      puts "You said: #{word}"
+    end
+  else
+    return "You said: #{string}"
+  end
 end
 
-playback(response)
+puts playback(response)
